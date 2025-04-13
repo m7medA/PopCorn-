709537966f032e20e2508ade6f46771e03cf4a32
mo7medA <modyayman246@gmail.com>
2025-04-13 16:41:35 +0200

    modify commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
-import StarRating from "./StarRating";
 
 // function Test() {
 //   const [movieRating, setMovieRating] = useState(0);
